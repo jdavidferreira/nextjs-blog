b162fef40b3b781848199dec92d8870c7cabe76d
David Ferreira <mrjdavidfg@gmail.com>
2020-09-28 22:48:09 -0500

    minor cleaning

--- pages/github_jobs/index.js
+++ pages/github_jobs/index.js
@@ -4,7 +4,6 @@ import Job from 'components/JobCard/JobCard'
 import Layout from 'components/Layout/Layout'
 import utilStyles from 'styles/utils.module.css'
 import styles from './index.module.css'
-import { isObjectEmpty } from 'utils/general'
 
 // Server-side rendered page
 export default function GithubJobList({ data }) {
@@ -32,19 +31,11 @@ export default function GithubJobList({ data }) {
   )
 }
 
-export async function getServerSideProps({ query }) {
+export async function getServerSideProps() {
   // Fetch data from external API
   const url = new URL(process.env.GITHUB_JOBS_API_URL)
 
   url.pathname = '/positions.json'
-
-  if (!isObjectEmpty(query)) {
-    for (const key in query) {
-      const value = query[key]
-
-      if (value != null && value !== '') url.searchParams.append(key, value)
-    }
-  }
 
   const res = await fetch(url)
   const data = await res.json()
